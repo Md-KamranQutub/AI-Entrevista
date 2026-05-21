3887941e51043f14024182b425e5039b5356fdfe
Md-KamranQutub <mdkamranqutub226@gmail.com>
2026-05-21 12:21:30 +0530

    Update Firebase configuration settings

--- client/src/utils/firebase.js
+++ client/src/utils/firebase.js
@@ -3,11 +3,12 @@ import {getAuth , GoogleAuthProvider} from 'firebase/auth'
 
 const firebaseConfig = {
   apiKey: import.meta.env.FIREBASE_API_KEY,
-  authDomain: "ai-entrevista.firebaseapp.com",
-  projectId: "ai-entrevista",
-  storageBucket: "ai-entrevista.firebasestorage.app",
-  messagingSenderId: "18417708758",
-  appId: "1:18417708758:web:54d953990c25b450ef2377"
+  authDomain: "ai-interview-43f72.firebaseapp.com",
+  projectId: "ai-interview-43f72",
+  storageBucket: "ai-interview-43f72.firebasestorage.app",
+  messagingSenderId: "303731558590",
+  appId: "1:303731558590:web:6aace962a6ba9e224976ef",
+  measurementId: "G-7K7FVR2151"
 };
 
 
