5ad967b456a30ae5d62f3f2d24582b4e36f0e5d3
Md-KamranQutub <mdkamranqutub226@gmail.com>
2026-05-21 12:17:48 +0530

    some minor changes

--- client/src/utils/firebase.js
+++ client/src/utils/firebase.js
@@ -2,12 +2,13 @@ import { initializeApp } from "firebase/app";
 import {getAuth , GoogleAuthProvider} from 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAn0L7hFNCDETGdmzy-Iy-akZXqrB9vITQ",
-  authDomain: "ai-entrevista.firebaseapp.com",
-  projectId: "ai-entrevista",
-  storageBucket: "ai-entrevista.firebasestorage.app",
-  messagingSenderId: "18417708758",
-  appId: "1:18417708758:web:54d953990c25b450ef2377"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+   authDomain: "ai-interview-43f72.firebaseapp.com",
+  projectId: "ai-interview-43f72",
+  storageBucket: "ai-interview-43f72.firebasestorage.app",
+  messagingSenderId: "303731558590",
+  appId: "1:303731558590:web:6aace962a6ba9e224976ef",
+  measurementId: "G-7K7FVR2151"
 };
 
 
